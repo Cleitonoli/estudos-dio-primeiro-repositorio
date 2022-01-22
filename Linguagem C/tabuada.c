bde24da09d0d35545c05cb050f75f1807428feab
Cleitonoli <cleitonoliveir@hotmail.com>
2022-01-22 18:05:28 -0300

    Inclusão da Pasta sobre Aulas Linguagem C

---
+++ Linguagem C/tabuada.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+	int main(){
+	int num;
+	int limite;
+	
+	
+		printf("\tTabuada\n");
+		printf("Qual Tabuada voce quer saber?\n ");
+		printf("Digite o Numero e tecle Enter\n");
+		scanf("%d",&num);
+	for(limite; limite<=10; limite++){
+	
+		
+		printf("\n%d X %d = %d\n",num,limite,num*limite);
+	
+	} 
+	
+	}
+	
